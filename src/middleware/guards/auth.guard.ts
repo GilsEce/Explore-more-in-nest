22645e12e70066b82d61ea5afa85bdabe83ac1cc
Gilbert Masangcay <gilbertmasangcay12@gmail.com>
2021-07-11 19:28:37 +0800

    modified exception

--- src/middleware/guards/auth.guard.ts
+++ src/middleware/guards/auth.guard.ts
@@ -1,12 +1,26 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
+
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return this.checkCredentials();
+    if(this.checkCredentials()){
+      return true;
+    }else{
+      throw new HttpException({
+        status: HttpStatus.UNAUTHORIZED,
+        error: 'Access Denied',
+      }, HttpStatus.UNAUTHORIZED);
+    }
   }
 
   checkCredentials():boolean{

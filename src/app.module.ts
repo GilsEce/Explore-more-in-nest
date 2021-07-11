844a7754136924e0ee498004e242fab6759ac771
Gilbert Masangcay <gilbertmasangcay12@gmail.com>
2021-07-11 17:35:18 +0800

    added logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { MoviesModule } from './modules/movies/movies.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [UsersModule, ProductsModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('users');
+  }
+}

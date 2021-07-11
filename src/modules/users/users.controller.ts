c6aebf3915ff86eabe73461c895717fed270f531
Gilbert Masangcay <gilbertmasangcay12@gmail.com>
2021-07-11 19:01:01 +0800

    added guard

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/middleware/guards/auth.guard';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
     @Get('/mac')
+    @UseGuards(AuthGuard)
     getUserMac():object{
         return this.usersService.getMacAdd();
     }

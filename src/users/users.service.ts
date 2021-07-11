27f25de0a80d2ea29394f598c11ae708b909337f
Gilbert Masangcay <gilbertmasangcay12@gmail.com>
2021-07-11 12:25:59 +0800

    Added getting of users mac address

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import getMAC, { isMAC } from 'getmac';
+import { Mac } from './interfaces/mac.interface';
+
+@Injectable()
+export class UsersService {
+    getMacAdd():object {
+        const macAddress:Mac = {mac_address:getMAC()};
+        return macAddress;
+    }
+}

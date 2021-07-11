258bf272f1eab70a6d35cd6fa67b1d29145e5066
Gilbert Masangcay <gilbertmasangcay12@gmail.com>
2021-07-11 16:52:19 +0800

    Shared Service

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
 import { MoviesController } from './movies.controller';
 import { ActionsService } from './services/actions/actions.service';
 import { DramaService } from './services/drama/drama.service';
 
 @Module({
+  imports:[UsersModule],
   controllers: [MoviesController],
   providers: [ActionsService, DramaService]
 })
